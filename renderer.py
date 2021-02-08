fb8332caf6c25cd5967fbd36d6c8625b67f15576
i____7d <i.second.7d@gmail.com>
2021-02-08 11:58:26 +0000

    gitignore

--- renderer.py
+++ renderer.py
@@ -1,4 +1,4 @@
 import colorama
 
-def renderer(plaList: dict, nodeList: dict, **kwargs):
+def render(plaList: dict, nodeList: dict, **kwargs):
     tiles = kwargs['tiles'] if 'tiles' in kwargs.keys() else None # array of (z,x,y)
