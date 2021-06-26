0a4ed1eaefe5ba569e8b5ebe3e57050145ad70a8
7d <61975820+iiiii7d@users.noreply.github.com>
2021-06-26 23:55:23 +0800

    v1.3: fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, The MRT Mapping Team'
 author = '7d'
 
 # The full version, including alpha/beta/rc tags
-release = "v"+renderer.__version__
+release = "v1.3"
 
 # -- General configuration ---------------------------------------------------
 
