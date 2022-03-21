ad9a8e552b7d7cde6effecb8e51e8d0f41123b23
7d <i.second.7d@gmail.com>
2022-03-21 11:31:47 +0800

    forgot base oops

--- renderer/objects/skinbuilder.py
+++ renderer/objects/skinbuilder.py
@@ -16,7 +16,7 @@ def _blend(h1: int, h2: int) -> int:
     r = hex(round(int(nh1[0:2], base=16) + int(nh2[0:2], base=16) / 2))[2:].zfill(2)
     g = hex(round(int(nh1[2:4], base=16) + int(nh2[2:4], base=16) / 2))[2:].zfill(2)
     b = hex(round(int(nh1[4:6], base=16) + int(nh2[4:6], base=16) / 2))[2:].zfill(2)
-    return int(r+g+b)
+    return int(r+g+b, base=16)
 
 class SkinBuilder:
     tile_size: int
