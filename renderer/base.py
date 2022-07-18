39fa98ee236331ef6ca310732477e6ddd713dce5
7d <i.second.7d@gmail.com>
2022-07-18 10:17:36 +0800

    change 15 to 8

--- renderer/base.py
+++ renderer/base.py
@@ -275,7 +275,7 @@ def render_part1_ray(components: ComponentList,
             
     log.info(f"Rendering components in {len(tile_coords)} tiles...")
     ph = ProgressHandler.remote()
-    cursor = 15
+    cursor = 8
     futures = [ray.remote(_pre_draw_components).remote(ph,
                                                        tile_coord,
                                                        components,
