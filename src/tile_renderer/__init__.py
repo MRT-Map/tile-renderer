6f901513a8f800ffdabf581234494bee57e35b2a
7d <61975820+iiiii7d@users.noreply.github.com>
2024-07-21 14:34:44 +0800

    multiprocessing

--- src/tile_renderer/__init__.py
+++ src/tile_renderer/__init__.py
@@ -1,4 +1,6 @@
 import functools
+import multiprocessing
+import os
 import subprocess
 from copy import copy
 from pathlib import Path
@@ -6,7 +8,7 @@ from typing import cast
 
 import rich
 import svg
-from rich.progress import track
+from rich.progress import track, Progress
 
 from tile_renderer.types.coord import TileCoord, Coord, Line
 from tile_renderer.types.pla2 import Component
@@ -24,19 +26,28 @@ def render_svg(components: list[Component], skin: Skin, zoom: int, offset: Coord
     return out
 
 
+def _f(args):
+    return _export_tile(*args)
+
+
 def render_tiles(
     components: list[Component],
     skin: Skin,
-    zoom_levels: set[int],
+    zoom: int,
     max_zoom_range: int,
     tile_size: int,
     offset: Coord = Coord(0, 0),
+    processes: int = os.cpu_count(),
 ) -> dict[TileCoord, bytes]:
     images = {}
-    for zoom in zoom_levels:
-        doc = render_svg(components, skin, zoom, offset)
-        for tile in track(Component.tiles(components, zoom, max_zoom_range), "[green] Exporting to PNG"):
-            images[tile] = _export_tile(doc, tile, max_zoom_range, skin, tile_size)
+    doc = render_svg(components, skin, zoom, offset)
+    tiles = Component.tiles(components, zoom, max_zoom_range)
+    with multiprocessing.Pool(processes=processes) as pool, Progress() as progress:
+        task_id = progress.add_task("[green] Exporting to PNG", total=len(tiles))
+
+        for tile, b in pool.imap(_f, ((doc, tile, max_zoom_range, skin, tile_size) for tile in tiles)):
+            images[tile] = b
+            progress.advance(task_id)
     return images
 
 
@@ -90,7 +101,9 @@ def _sort_styling(
     return sorted(styling, key=functools.cmp_to_key(cast(any, sort_fn)))
 
 
-def _export_tile(doc: svg.SVG, tile: TileCoord, max_zoom_range: int, skin: Skin, tile_size: int) -> bytes:
+def _export_tile(
+    doc: svg.SVG, tile: TileCoord, max_zoom_range: int, skin: Skin, tile_size: int
+) -> tuple[TileCoord, bytes]:
     doc2 = copy(doc)
     bounds = tile.bounds(max_zoom_range)
     doc2.viewBox = svg.ViewBoxSpec(
@@ -99,7 +112,6 @@ def _export_tile(doc: svg.SVG, tile: TileCoord, max_zoom_range: int, skin: Skin,
         width=bounds.x_max - bounds.x_min,
         height=bounds.y_max - bounds.y_min,
     )
-    Path("./out.svg").write_text(str(doc2))
     p = subprocess.Popen(
         [
             "resvg",
@@ -119,7 +131,7 @@ def _export_tile(doc: svg.SVG, tile: TileCoord, max_zoom_range: int, skin: Skin,
         stdin=subprocess.PIPE,
         stderr=subprocess.PIPE,
     )
-    return p.communicate(input=str(doc2).encode("utf-8"))[0]
+    return tile, p.communicate(input=str(doc2).encode("utf-8"))[0]
 
 
 def _filter_text_list(text_list: list[tuple[Line, svg.Element]]) -> list[svg.Element]:
--- tests/enc.py
+++ tests/enc.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from tile_renderer import Skin, render_svg, render_tiles
@@ -7,10 +8,10 @@ from tile_renderer.types.pla2 import Pla2File
 def main():
     a = Pla2File.from_file(Path(__file__).parent / "enc.pla2.msgpack")
     skin = Skin.default()
-    (Path(__file__).parent / "out.svg").write_text(str(render_svg(a.components, skin, 0)))
-    # Path(f"/tmp/tile-renderer").mkdir(exist_ok=True)
-    # for tile, b in render_tiles(a.components, skin, {0}, 32).items():
-    #     Path(f"/tmp/tile-renderer/{tile}.png").write_bytes(b)
+    # (Path(__file__).parent / "out.svg").write_text(str(render_svg(a.components, skin, 0)))
+    Path(f"/tmp/tile-renderer").mkdir(exist_ok=True)
+    for tile, b in render_tiles(a.components, skin, 0, 32, 256, processes=os.cpu_count() * 2).items():
+        Path(f"/tmp/tile-renderer/{tile}.png").write_bytes(b)
 
 
 if __name__ == "__main__":
