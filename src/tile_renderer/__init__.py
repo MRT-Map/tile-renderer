abe4ef4f05f4da1c65979fe3ef91c52d4a0a7ff6
7d <61975820+iiiii7d@users.noreply.github.com>
2024-07-21 18:47:17 +0800

    fix indent

--- src/tile_renderer/__init__.py
+++ src/tile_renderer/__init__.py
@@ -112,11 +112,7 @@ def _export_tile(
     doc: str, tile: TileCoord, max_zoom_range: int, skin: Skin, tile_size: int
 ) -> tuple[TileCoord, bytes]:
     bounds = tile.bounds(max_zoom_range)
-    doc = doc.value
-        .replace("<|min_x|>", bounds.x_min)
-        .replace("<|min_y|>", bounds.y_min)
-        .replace("<|width|>", bounds.x_max - bounds.x_min)
-        .replace("<|height|>", bounds.y_max - bounds.y_min)
+    doc = doc.value.replace("<|min_x|>", bounds.x_min).replace("<|min_y|>", bounds.y_min).replace("<|width|>", bounds.x_max - bounds.x_min).replace("<|height|>", bounds.y_max - bounds.y_min)
     
     p = subprocess.Popen(
         [
