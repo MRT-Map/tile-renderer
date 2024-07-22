f0b26badce78652c0ed5898af9ba3b07295f9f84
7d <61975820+iiiii7d@users.noreply.github.com>
2024-07-23 07:39:32 +0800

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -11,10 +11,10 @@ def main():
     a = Pla2File.from_file(Path(__file__).parent / "CAR.pla2.msgpack")
     skin = Skin.default()
     for zoom in (0, 1, 2, 3, 4, 5, 6, 7, 8, 9):
-        (Path(__file__).parent / f"out{zoom}.svg").write_text(str(render_svg(a.components, skin, zoom)))
-        # Path("/tmp/tile-renderer").mkdir(exist_ok=True)
-        # for tile, b in render_tiles(a.components, skin, zoom, 32, 256).items():
-        #     Path(f"/tmp/tile-renderer/{tile}.png").write_bytes(b)
+        # (Path(__file__).parent / f"out{zoom}.svg").write_text(str(render_svg(a.components, skin, zoom)))
+        Path("/tmp/tile-renderer").mkdir(exist_ok=True)
+        for tile, b in render_tiles(a.components, skin, zoom, 32, 256).items():
+            Path(f"/tmp/tile-renderer/{tile}.png").write_bytes(b)
 
 
 if __name__ == "__main__":
