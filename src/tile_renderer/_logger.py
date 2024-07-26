e2a4d6cdf0bf89103bfcce00354bb116f15fe9bb
7d <61975820+iiiii7d@users.noreply.github.com>
2024-07-26 18:01:45 +0800

    add LOG environment variable

--- src/tile_renderer/_logger.py
+++ src/tile_renderer/_logger.py
@@ -1,9 +1,10 @@
 import logging
+import os
 
 from rich.logging import RichHandler
 
 logging.basicConfig(
-    level="NOTSET",
+    level=os.environ.get("LOG", "INFO").upper(),
     format="%(message)s",
     # datefmt=" ",
     handlers=[RichHandler(markup=True, show_path=False)],
