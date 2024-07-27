9e902181ef42c12c5bf7b1e6d64038b835cd14b0
7d <61975820+iiiii7d@users.noreply.github.com>
2024-07-27 14:21:19 +0800

    import everything in __init__ + actually fix logger

---
+++ src/tile_renderer/__init__.py
@@ -0,0 +1,9 @@
+import tile_renderer.__about__
+import tile_renderer.colour
+import tile_renderer.component_to_svg
+import tile_renderer.coord
+import tile_renderer.pla1to2
+import tile_renderer.pla2
+import tile_renderer.render_svg
+import tile_renderer.render_tiles
+import tile_renderer.skin
--- src/tile_renderer/_logger.py
+++ src/tile_renderer/_logger.py
@@ -2,14 +2,14 @@ import logging
 import os
 import sys
 
+from rich.console import Console
 from rich.logging import RichHandler
 
 logging.basicConfig(
     level=os.environ.get("LOG", "INFO").upper(),
     format="%(message)s",
     # datefmt=" ",
-    handlers=[RichHandler(markup=True, show_path=False)],
-    stream=sys.stdout,
+    handlers=[RichHandler(markup=True, show_path=False, console=Console(file=sys.stderr))],
 )
 
 log = logging.getLogger("rich")
